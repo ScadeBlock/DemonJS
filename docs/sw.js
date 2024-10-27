ff163646136df6efc376b143865de249107b7fdc
D <89845150+ScadeBlock@users.noreply.github.com>
2024-10-27 17:54:29 +0700

    Update sw.js

--- docs/sw.js
+++ docs/sw.js
@@ -1,9 +1,9 @@
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 const filesToCache = [
-    {url: '/index.html', revision: '102724.2'},
-    {url: '/dist/bundle.min.js', revision: '102724.2'},
-    {url: '/dist/source-map.js', revision: '102724.2'},
-    {url: '/dist/ic.png', revision: '102724.2'}
+    {url: '/index.html', revision: '102724.3'},
+    {url: 'https://cdn.jsdelivr.net/gh/ScadeBlock/DemonJS@main/docs/dist/bundle.min.js', revision: '102724.3'},
+    {url: 'https://cdn.jsdelivr.net/gh/ScadeBlock/DemonJS@main/docs/dist/source-map.js', revision: '102724.3'},
+    {url: '/dist/ic.png', revision: '102724.3'}
 
 ];
 workbox.precaching.precacheAndRoute(filesToCache);
